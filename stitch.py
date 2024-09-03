6791ffd3ce61954a5c0aac3576493408be653bec
betanight <betanight2002@gmail.com>
2024-09-03 14:55:21 -0700

    fixed stitch
    Originally stitch was stitching tiles that were 50x50 tiles which is not divisible by 225 so the image was being cut off. This will be important to note in the future.

--- stitch.py
+++ stitch.py
@@ -36,8 +36,8 @@ def stitch(output_dir, tile_size, x_tiles, y_tiles, output_image_path):
 # Parameters for the stitching function
 output_dir = 'output_tiles'  # Directory where the tiles are saved
 tile_size = 15  # Size of each tile (must match the tile size used during slicing)
-x_tiles = 3  # Number of tiles along the x-axis
-y_tiles = 3  # Number of tiles along the y-axis
+x_tiles = 15  # Number of tiles along the x-axis
+y_tiles = 15  # Number of tiles along the y-axis
 output_image_path = 'stitched_image.png'  # Path where the stitched image will be saved
 
 # Run the stitching function
